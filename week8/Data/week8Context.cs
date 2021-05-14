59c68ddee3e4af1549906f4329d4e098e080e11b
Vlad <vlad.but@emanuel.ro>
2021-05-14 12:56:35 +0300

    Add project files.

---
+++ week8/Data/week8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using week8.Models;
+
+namespace week8.Data
+{
+    public class week8Context : DbContext
+    {
+        public week8Context (DbContextOptions<week8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<week8.Models.Movie> Movie { get; set; }
+    }
+}
